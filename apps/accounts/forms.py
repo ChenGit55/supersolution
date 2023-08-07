d2b669f8c2fac539b752b86aa7cfa998898fa109
xen <chenpsilva@gmail.com>
2023-08-06 21:44:44 -0300

    texto de ajuda adicionado ao campo de senha

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -3,7 +3,7 @@ from .models import CustomUser
 from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput, help_text='Senha de no mínimo 8 caracteres contendo ao menos uma letra e um número!')
     password2 = forms.CharField(label='Confirmação de senha', widget=forms.PasswordInput)
     is_staff = forms.BooleanField(label='Staff', required=False)
 
@@ -11,6 +11,10 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'is_staff')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = "Senha com 8 caracteres ou mais contendo pelo menos uma letra e um número."
 
 class CustomUserChangeForm(UserChangeForm):
 
