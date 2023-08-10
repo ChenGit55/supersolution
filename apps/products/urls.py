960887c018d26c203b925b1401b8a455c8e665f9
xen <chenpsilva@gmail.com>
2023-08-09 22:22:25 -0300

    criado sidebar simples para referência

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import product_list_view, new_product_view
 
 urlpatterns = [
-    path('', product_list_view, name="home"),
+    path('', product_list_view, name="products"),
     path('new/', new_product_view, name="new-product")
 ]
