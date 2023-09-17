351383a666b7ab4fafe2dcd393ee7696d7aaf260
xen <chenpsilva@gmail.com>
2023-09-17 12:48:22 -0300

    collectstatic

--- staticfiles/pos/js/pos.js
+++ staticfiles/pos/js/pos.js
@@ -1,11 +1,16 @@
-console.log('test')
-var api = document.getElementById('api');
-var apiUrl = api.getAttribute('data-api-url');
+const userID = document.getElementById('user');
+const apiProducts = document.getElementById('apiproducts');
+const apiProdcutsUrl = apiProducts.getAttribute('data-api-url');
+const apiSales = document.getElementById('apisales');
+const apiSalesUrl = apiSales.getAttribute('data-api-url');
 var priceField = document.getElementById('product-price');
 var quantityField = document.getElementById('product-quantity');
 var totProductField = document.getElementById('product-total');
 const inoviceList = document.getElementById('inovice-list');
 const addProductButton = document.getElementById('add-product');
+var productsList = [];
+var inoviceTotal = document.getElementById('inovice-total');
+var currentTotal = 0
 
 // currency format
 function fCurrency(value) {
@@ -17,7 +22,7 @@ function fCurrency(value) {
   }
 }
 
-fetch(apiUrl)
+fetch(apiProdcutsUrl)
   .then(function(response) {
     // Verifique se a resposta da API está OK (código de status 200)
     if (!response.ok) {
@@ -29,7 +34,6 @@ fetch(apiUrl)
   .then(function(data) {
     // Agora, 'data' contém o objeto JSON da API
     console.log(data);
-
     
     // criando menu select com os dados da api
     var productSelect = document.getElementById('product-select');
@@ -46,17 +50,72 @@ fetch(apiUrl)
     })
 
     addProductButton.addEventListener('click', function() {
-      var productTotal = quantityField.value*parseFloat(priceField.value.replace(',','.'));
-      var inoviceItem = document.createElement('li');
-      var productTitle = productSelect.options[productSelect.selectedIndex].text;
-      inoviceItem.textContent = `${productTitle} Preço R$ ${fCurrency(priceField.value)} Qantidade ${quantityField.value} Total: ${fCurrency(productTotal)}`;
-      inoviceList.appendChild(inoviceItem);
+      var selectedProductIndex = productSelect.selectedIndex;
+      if(Boolean(priceField.value) && Boolean(quantityField.value) && selectedProductIndex !== -1 ){        
+        var productTotal = quantityField.value*parseFloat(priceField.value.replace(',','.'));
+        var inoviceItem = document.createElement('li');
+        var productTitle = productSelect.options[productSelect.selectedIndex].text;
 
-      priceField.value = '';
-      quantityField.value = '';
+        var selectedProduct = data[selectedProductIndex];
+        var inputPrice = parseFloat(priceField.value.replace(',','.'));
+        var inputQuantity = quantityField.value;
+        var inputTotal = productTotal;
+
+        currentTotal += productTotal;
+        inoviceTotal.textContent = currentTotal
+
+        productsList.push({
+          product: selectedProduct.title,
+          quantity: inputQuantity,
+          price: inputPrice,
+          total: inputTotal,
+        });
+        console.log(productsList);
+        
+        inoviceItem.textContent = `${productTitle} Preço R$ ${fCurrency(priceField.value)} Qantidade ${quantityField.value} Total: ${fCurrency(productTotal)}`;
+        inoviceList.appendChild(inoviceItem);
+
+        priceField.value = '';
+        quantityField.value = '';
+      } else {
+
+      }
     })
-
   })
   .catch(function(error) {
     console.error('Erro: ' + error.message);
   });
+
+  // Adicione um evento de clique ao botão "Finalizar Venda"
+document.getElementById('close-inovice').addEventListener('click', function() {
+  if (productsList.length > 0) {
+    // Crie a venda completa com todos os produtos selecionados
+    var vendaCompleta = {
+      user: userID, // Substitua pelo ID do usuário adequado
+      items: productsList
+    };
+
+    fetch(apiSalesUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(vendaCompleta),
+    })
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error('Erro ao criar a venda');
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      console.log('Venda criada com sucesso:', data);
+      // Faça algo com a resposta do servidor, se necessário
+    })
+    .catch(function(error) {
+      console.error('Erro ao criar a venda:', error);
+    });
+  } else {
+    console.error('Nenhum produto selecionado. Selecione pelo menos um produto antes de finalizar a venda.');
+  }
+});
