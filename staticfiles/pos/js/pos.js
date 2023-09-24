70c834fab82d61c907cced057abb9cab3257c71c
xen <chenpsilva@gmail.com>
2023-09-24 19:57:29 -0300

    collectstatic

--- staticfiles/pos/js/pos.js
+++ staticfiles/pos/js/pos.js
@@ -1,18 +1,19 @@
-const userID = document.getElementById('user');
-const apiProducts = document.getElementById('apiproducts');
-const apiProdcutsUrl = apiProducts.getAttribute('data-api-url');
-const apiSales = document.getElementById('apisales');
-const apiSalesUrl = apiSales.getAttribute('data-api-url');
-var priceField = document.getElementById('product-price');
-var quantityField = document.getElementById('product-quantity');
-var totProductField = document.getElementById('product-total');
+const userElement = document.getElementById('user');
+const productSelect = document.getElementById('product-select');
+const priceField = document.getElementById('product-price');
+const quantityField = document.getElementById('product-quantity');
 const inoviceList = document.getElementById('inovice-list');
 const addProductButton = document.getElementById('add-product');
-var productsList = [];
-var inoviceTotal = document.getElementById('inovice-total');
-var currentTotal = 0
+const inoviceTotal = document.getElementById('inovice-total');
+const closeInvoiceButton = document.getElementById('close-inovice');
 
-// currency format
+const apiProdcutsUrl = document.getElementById('apiproducts').getAttribute('data-api-url');
+const userID = userElement ? userElement.value : null;
+
+const productsList = [];
+let currentTotal = 0;
+
+// Currency format function
 function fCurrency(value) {
   if (typeof value === 'string') {
     value = value.replace(',', '.').replace(/[^0-9.]/g, '');
@@ -22,100 +23,117 @@ function fCurrency(value) {
   }
 }
 
+// function to get the CSRF token
+function getCSRFToken() {
+  const cookieValue = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('csrftoken='))
+    .split('=')[1];
+  return cookieValue;
+}
+
+// shows up the product to client
+function addProduct(productTitle, price, quantity, productTotal) {
+  const inoviceProduct = document.createElement('li');
+  inoviceProduct.textContent = `${productTitle} - ${fCurrency(price)} x ${quantity} - Total: ${fCurrency(productTotal)}`;
+  inoviceList.appendChild(inoviceProduct);
+}
+
+// Fetch products from the API
 fetch(apiProdcutsUrl)
-  .then(function(response) {
-    // Verifique se a resposta da API está OK (código de status 200)
+  .then(response => {
     if (!response.ok) {
       throw new Error('API REQUEST ERROR!');
     }
-    // Parse a resposta JSON
     return response.json();
   })
-  .then(function(data) {
-    // Agora, 'data' contém o objeto JSON da API
-    console.log(data);
-    
-    // criando menu select com os dados da api
-    var productSelect = document.getElementById('product-select');
-    data.forEach(function(product){
-        var option = document.createElement('option');
-        option.text = `${product.title}`;
-        option.value = product.price;
-        productSelect.appendChild(option);        
-    });
-    productSelect.addEventListener('change', function() {
-      var selectedPrice = productSelect.options[productSelect.selectedIndex].value;
-      priceField.value = selectedPrice.replace('.',',');
-      quantityField.value = 1
-    })
+  .then(data => {
+    //select menu with API data
+    data.forEach(product => {
+      const option = document.createElement('option');
+      option.text = product.title;
+      option.value = product.price;
+      option.setAttribute('data-id', product.id);
+      productSelect.appendChild(option);
+    });   
 
-    addProductButton.addEventListener('click', function() {
-      var selectedProductIndex = productSelect.selectedIndex;
-      if(Boolean(priceField.value) && Boolean(quantityField.value) && selectedProductIndex !== -1 ){        
-        var productTotal = quantityField.value*parseFloat(priceField.value.replace(',','.'));
-        var inoviceItem = document.createElement('li');
-        var productTitle = productSelect.options[productSelect.selectedIndex].text;
+    addProductButton.addEventListener('click', () => {
+      const selectedProductIndex = productSelect.selectedIndex;
+      if (
+        Boolean(priceField.value) &&
+        Boolean(quantityField.value) &&
+        selectedProductIndex !== -1
+      ) {
+        const productID = productSelect.options[selectedProductIndex].getAttribute('data-id');
+        const productTitle = productSelect.options[productSelect.selectedIndex].text;
+        const inputPrice = parseFloat(priceField.value.replace(',', '.'));
+        const inputQuantity = quantityField.value;
+        const productTotal = quantityField.value * parseFloat(priceField.value.replace(',', '.'));
 
-        var selectedProduct = data[selectedProductIndex];
-        var inputPrice = parseFloat(priceField.value.replace(',','.'));
-        var inputQuantity = quantityField.value;
-        var inputTotal = productTotal;
-
-        currentTotal += productTotal;
-        inoviceTotal.textContent = currentTotal
+        // Criar campos dinâmicos com nomes únicos
 
         productsList.push({
-          product: selectedProduct.title,
-          quantity: inputQuantity,
-          price: inputPrice,
-          total: inputTotal,
+            id: productID,
+            product: productTitle,
+            quantity: inputQuantity,
+            price: inputPrice,
         });
-        console.log(productsList);
         
-        inoviceItem.textContent = `${productTitle} Preço R$ ${fCurrency(priceField.value)} Qantidade ${quantityField.value} Total: ${fCurrency(productTotal)}`;
-        inoviceList.appendChild(inoviceItem);
+        addProduct(productTitle, priceField.value, quantityField.value, productTotal);
+            
+        //sum all products totals
+        currentTotal += productTotal;
+        inoviceTotal.textContent = fCurrency(currentTotal);
 
         priceField.value = '';
         quantityField.value = '';
-      } else {
-
       }
-    })
+    });
   })
-  .catch(function(error) {
+  .catch(error => {
     console.error('Erro: ' + error.message);
   });
 
-  // Adicione um evento de clique ao botão "Finalizar Venda"
-document.getElementById('close-inovice').addEventListener('click', function() {
+// Close invoice button click event
+closeInvoiceButton.addEventListener('click', () => {
   if (productsList.length > 0) {
-    // Crie a venda completa com todos os produtos selecionados
-    var vendaCompleta = {
-      user: userID, // Substitua pelo ID do usuário adequado
-      items: productsList
-    };
+    const salesForm = document.getElementById('new-sale-form');
+    document.getElementById('user').value = userID;
+    productsList.forEach((product, index) => {
 
-    fetch(apiSalesUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(vendaCompleta),
-    })
-    .then(function(response) {
-      if (!response.ok) {
-        throw new Error('Erro ao criar a venda');
-      }
-      return response.json();
-    })
-    .then(function(data) {
-      console.log('Venda criada com sucesso:', data);
-      // Faça algo com a resposta do servidor, se necessário
-    })
-    .catch(function(error) {
-      console.error('Erro ao criar a venda:', error);
+      const inputID = document.createElement('input');
+      inputID.type = 'hidden';
+      inputID.name = `product-${index+1}-id`;
+      inputID.value = product.id;
+      salesForm.appendChild(inputID);
+      
+      const inputProduct = document.createElement('input');
+      inputProduct.type = 'hidden';
+      inputProduct.name = `product-${index + 1}-title`;
+      inputProduct.value = product.product;
+      salesForm.appendChild(inputProduct);
+
+      const inputPrice = document.createElement('input');
+      inputPrice.type = 'hidden';
+      inputPrice.name = `product-${index + 1}-price`;
+      inputPrice.value = product.price;
+      salesForm.appendChild(inputPrice);
+
+      const inputQuantity = document.createElement('input');
+      inputQuantity.type = 'hidden';
+      inputQuantity.name = `product-${index + 1}-quantity`;
+      inputQuantity.value = product.quantity;
+      salesForm.appendChild(inputQuantity);
     });
+    salesForm.submit()
   } else {
     console.error('Nenhum produto selecionado. Selecione pelo menos um produto antes de finalizar a venda.');
   }
 });
+
+ //shows up the default price when change the selected product
+ productSelect.addEventListener('change', () => {
+  const selectedPrice = productSelect.options[productSelect.selectedIndex].value;
+  priceField.value = selectedPrice.replace('.', ',');
+  quantityField.value = 1;
+}); 
