1857ffb59a043c240f820a215184eed8c1da445a
xen <chenpsilva@gmail.com>
2023-10-01 20:37:28 -0300

    criação de usuário sem loja definida

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,7 +4,7 @@ from apps.stores.models import Store
 
 class CustomUser(AbstractUser):
     username = models.CharField('nome', max_length=128, unique=True)
-    store = models.ForeignKey(Store, on_delete=models.SET_NULL, default='', null=True)
+    store = models.ForeignKey(Store, on_delete=models.SET_NULL, default='', null=True, blank=True)
     USERNAME_FIELD = 'username'
 
     def __str__(self):
