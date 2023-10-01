8dfd93bf080ff980edf48fe0dc10ed1cd75cd710
xen <chenpsilva@gmail.com>
2023-10-01 17:12:50 -0300

    usuário vinculado a loja

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from apps.stores.models import Store
 
 class CustomUser(AbstractUser):
     username = models.CharField('nome', max_length=128, unique=True)
+    store = models.ForeignKey(Store, on_delete=models.SET_NULL, null=True)
     USERNAME_FIELD = 'username'
 
     def __str__(self):
