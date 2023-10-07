a45bd9c8afb15194797b2b8032eb2aa998db98cb
xen <chenpsilva@gmail.com>
2023-10-07 12:27:08 -0300

    usuário só pode ser criado depois que for vinculado a uma loja

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import CustomUserCreationForm
+from apps.stores.models import Store
 
 def signup_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
+        stores = Store.objects.all()
+        if stores.exists() and form.is_valid():
+            msg = ''
             user = form.save(commit=False)
             user.is_staff = True
             user.save()
             return redirect('login')
+        else:
+            msg = "Precisa cadastrar uma loja primeiro!"
     else:
+        msg = '' 
         form = CustomUserCreationForm()
-    context = { 'form' : form }
+    context = {
+         'form' : form,
+         'msg' : msg,
+         'stores' : stores,
+    }
     return render(request, 'accounts/signup.html', context)
 
 def login_view(request):
