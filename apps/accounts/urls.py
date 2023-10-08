3ed5503fd0aa532b728c0329c08fbe610d30f7fd
xen <chenpsilva@gmail.com>
2023-10-08 19:39:56 -0300

    erro de digitação corrigido

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('logout/', logout_view, name='logout'),
-    path('profile', profile_view, name='profile'),
+    path('profile/', profile_view, name='profile'),
 ]
