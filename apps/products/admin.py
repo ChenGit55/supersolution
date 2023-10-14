e5cf3a491730fa74165485b607ee9ead6e27bb30
xen <chenpsilva@gmail.com>
2023-10-13 21:52:44 -0300

    Modelo de produtos adicionado ao django admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Product
 
-admin.register(Product)
+admin.site.register(Product)
--- apps/products/models.py
+++ apps/products/models.py
@@ -17,7 +17,6 @@ class Product(models.Model):
     description = models.TextField()
     code = models.CharField(max_length=50, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='produtos')
     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, related_name='produtos')
 
