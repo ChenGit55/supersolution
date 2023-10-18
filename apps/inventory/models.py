d3506046a8f002a8f3a0bedd44138bf14d4c55f4
xen <chenpsilva@gmail.com>
2023-10-18 06:25:24 -0300

    Backup de produtos e algumas alterações para teste na lógica de estoque

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -3,19 +3,25 @@ from apps.products.models import Product
 from apps.stores.models import Store
 
 
+
+
+
 class Stock(models.Model):
+
     name = models.CharField(max_length=110)
 
     def __str__(self):
         return self.name
 
 
+class Location(models.Model):
+
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True, blank=True)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, null=True, blank=True)
+
+
 class Item(models.Model):
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.IntegerField()
-
-    def __str__(self) -> str:
-        return f'{self.product} - Quantidade: {self.quantity} - Loja: {self.store}'
--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from apps.stores.models import Store
 from .models import Item, Stock
+from .forms import ItemForm
 
+@login_required
 def inventory_view(request):
     all_items = Item.objects.all()
+    stores = Store.objects.all()
     stocks = Stock.objects.all()
+    form = ItemForm(request.POST)
+
+    items_by_store = {}
+
+    for store in stores:
+        items = Item.objects.filter(store=store)
+        items_by_store[store] = items
+
+    if request.method == 'POST':
+        form.save()
     context = {
         'all_items' : all_items,
+        'stores' : stores,
+        'items_by_store' : items_by_store,
         'stocks' : stocks,
+        'form' : form,
     }
     return render(request, 'inventory/inventory.html', context)
