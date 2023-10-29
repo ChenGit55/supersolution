8cde2061718716afdef7ae39d558cec72e50b2e9
xen <chenpsilva@gmail.com>
2023-10-29 09:21:13 -0300

    Filtra todos os produtos pelo local escolhido pelo usuário

--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from apps.stores.models import Store
+from apps.accounts.models import CustomUser
 from .models import Item, Stock
 from .forms import ItemForm
 
 @login_required
 def inventory_view(request):
     items = Item.objects.all()
+    stores = Store.objects.all()
+    stocks = Stock.objects.all()
+    location_items = ""
+
+    if request.method == 'POST':
+        current_location = request.POST.get('location')
+        if current_location== "":
+            current_location= "Loja/Estoque, não definido"
+        location_items = Item.objects.filter(location=current_location).all()
+    else:
+        current_location = ""
+
     context = {
         'items' : items,
+        'stores' : stores,
+        'stocks' : stocks,
+        'current_location' : current_location,
+        'location_items' : location_items,
     }
     return render(request, 'inventory/inventory.html', context)
 
@@ -24,17 +41,17 @@ def add_product_view(request):
 
         if form.is_valid():
             product_id = request.POST.get('product')
-            location = request.POST.get('location')
-            if location == "" :
-                location = "Loja/Estoque, não definido"
+            current_location= request.POST.get('location')
+            if current_location== "" :
+                current_location= "Loja/Estoque, não definido"
             quantity = int(request.POST['quantity'])
-            existing_item = Item.objects.filter(product__id=product_id, location=location).first()
+            existing_item = Item.objects.filter(product__id=product_id, location=current_location).first()
 
             if existing_item:
                 existing_item.quantity += quantity
                 existing_item.save()
             else:
-                Item.objects.create(product_id=product_id, location=location, quantity=quantity)
+                Item.objects.create(product_id=product_id, location=current_location, quantity=quantity)
 
         return redirect('add-product')
 
@@ -51,13 +68,14 @@ def transfer_product_view(request):
     items = Item.objects.all()
     stores = Store.objects.all()
     stocks = Stock.objects.all()
+    location_items = ""
     if request.method == 'POST':
-        location = request.POST.get('location')
-        location_items = Item.objects.filter(location=location).all()
-        print(location_items)
+        current_location= request.POST.get('location')
+        location_items = Item.objects.filter(location=current_location).all()
     context = {
         'items' : items,
         'stores' : stores,
         'stocks' : stocks,
+        'location_items' : location_items,
     }
     return render(request, 'inventory/transfer-product.html', context)
